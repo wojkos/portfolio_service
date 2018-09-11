831d05027e86bac4ce140c46bcb1017a6676e9ff
Wojtek Kostanski <wojtek@tappest.com>
2018-09-11 21:58:43 +0200

    add seeds and display porfolio items

--- PortfolioService/db/seeds.rb
+++ PortfolioService/db/seeds.rb
@@ -1,4 +1,27 @@
-Skill.create(title: 'Rails', skill_percent: 60 )
-Skill.create(title: 'HTML', skill_percent: 80 )
-Skill.create(title: 'Wordpress', skill_percent: 70 )
-Skill.create(title: 'Vue', skill_percent: 30 )
+Skill.create!(title: 'Rails', skill_percent: 60 )
+Skill.create!(title: 'HTML', skill_percent: 80 )
+Skill.create!(title: 'Wordpress', skill_percent: 70 )
+Skill.create!(title: 'Vue', skill_percent: 30 )
+
+puts 'Created 4 skills'
+
+10.times do |post|
+  Post.create!(
+    title: "My post title #{post}",
+    body: 'Lorem ipsum doloros edum noe dritut bre. Goden nolo fis est duri.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis eros eget justo facilisis tempus id id risus. Pellentesque orci est, pulvinar in vestibulum eu, mattis et nulla. Praesent bibendum nisl eget justo bibendum blandit. Fusce sit amet erat sed quam finibus fermentum in nec risus. Sed dui eros, convallis nec fermentum imperdiet, volutpat venenatis sapien. Aliquam porta arcu in placerat malesuada. Integer a sagittis neque. Fusce suscipit eget mauris quis auctor. Cras nec risus nec nisl convallis viverra. Vestibulum convallis odio a luctus scelerisque. Phasellus sed pretium mauris. Mauris non ante nec turpis viverra sollicitudin.'
+  )
+end
+
+puts 'Created 10 skills'
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio item #{portfolio_item}",
+    subtitle: "Portfolio subtitle #{portfolio_item}",
+    body: 'Lorem ipsum doloros edum noe dritut bre. Goden nolo fis est duri.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis eros eget justo facilisis tempus id id risus. Pellentesque orci est, pulvinar in vestibulum eu, mattis et nulla. Praesent bibendum nisl eget justo bibendum blandit. Fusce sit amet erat sed quam finibus fermentum in nec risus. Sed dui eros, convallis nec fermentum imperdiet, volutpat venenatis sapien. Aliquam porta arcu in placerat malesuada. Integer a sagittis neque. Fusce suscipit eget mauris quis auctor. Cras nec risus nec nisl convallis viverra. Vestibulum convallis odio a luctus scelerisque. Phasellus sed pretium mauris. Mauris non ante nec turpis viverra sollicitudin.',
+    main_image: 'http://placehold.it/600x400',
+    thumb_image: 'http://placehold.it/350x150'
+  )
+end
+
+puts 'Created 9 portfolio items'
