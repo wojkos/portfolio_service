3e5b577f5bd22ca6dc65b485369e92a5cb0c38e7
Wojtek Kostanski <wojtek@tappest.com>
2018-09-12 00:13:30 +0200

    deal with routes

--- PortfolioService/config/routes.rb
+++ PortfolioService/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolio#show', as: 'portfolio_show'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
+  resources :posts, except: [:index]
+  get 'blog', to: 'posts#index'
 
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get '*path', to: redirect('/')
 end
