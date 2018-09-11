09c062ce35a30aa95052e44d4a1487da8e424cdd
Wojtek Kostanski <wojtek@tappest.com>
2018-09-11 23:41:53 +0200

    add rest portfolio CRUD

--- PortfolioService/app/controllers/portfolios_controller.rb
+++ PortfolioService/app/controllers/portfolios_controller.rb
@@ -1,6 +1,59 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_item_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_item_params)
+        format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show; end
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Item was successfully destroyed.' }
+    end
+  end
+
+  private
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def portfolio_item_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image, :main_image)
+    end
+
+
 end
