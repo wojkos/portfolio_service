2b11e26648877ea3d636263af7dafcbbfe7c3284
Wojtek Kostanski <wojtek@tappest.com>
2018-09-13 22:01:09 +0200

    add slug for portfolios and posts

--- PortfolioService/db/migrate/20180912220018_add_slug_to_post_and_portfolio.rb
+++
@@ -1,8 +0,0 @@
-class AddSlugToPostAndPortfolio < ActiveRecord::Migration[5.1]
-  def change
-    add_column :posts, :slug, :string
-    add_index :posts, :slug, unique: true
-    add_column :portfolios, :slug, :string
-    add_index :portfolios, :slug, unique: true
-  end
-end
