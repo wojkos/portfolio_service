3c3e2523e241e4f1a8a2cc7d94ede0a98cdbd4f1
Wojtek Kostanski <wojtek@tappest.com>
2018-09-15 21:49:13 +0200

    set defaults in model

--- PortfolioService/app/models/portfolio.rb
+++ PortfolioService/app/models/portfolio.rb
@@ -4,4 +4,11 @@ class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialize :set_defaults_images
+
+  def set_defaults_images
+    self.main_image ||= 'http://placehold.it/600x400'
+    self.thumb_image ||= 'http://placehold.it/350x200'
+  end
 end
