a99cdf5d39e1a5f2a8b5b44fa1480dce41316abf
Wojtek Kostanski <wojtek@tappest.com>
2018-09-25 23:22:10 +0200

    working on layout partials and helpers

--- PortfolioService/app/controllers/posts_controller.rb
+++ PortfolioService/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy, :toggle_status]
+  layout 'blog'
 
   def index
     @posts = Post.all
--- PortfolioService/app/helpers/application_helper.rb
+++ PortfolioService/app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def login_link_helper
+    if current_user.is_a?(User)
+      link_to("Logout", destroy_user_session_path, method: :delete)
+    else
+      (link_to("Login", new_user_session_path)) + ' | ' + 
+        (link_to("Register", new_user_registration_path))
+    end
+  end
+  
+  def source_helper
+    if session[:source]
+      content_tag(:p) do
+        "Thanks, for visit from #{session[:source]}"
+      end
+    end
+  end
 end
