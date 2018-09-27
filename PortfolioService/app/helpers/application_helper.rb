f0c9a4fc31f875e3fec5b8da8328f0af89bd9a7c
Wojtek Kostanski <wojtek@tappest.com>
2018-09-27 22:25:14 +0200

    style home page

--- PortfolioService/app/helpers/application_helper.rb
+++ PortfolioService/app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def login_link_helper
     if current_user.is_a?(GuestUser)
-      (link_to("Login", new_user_session_path)) + ' | ' + 
-        (link_to("Register", new_user_registration_path))
+      (link_to("Login", new_user_session_path, class: 'nav-link')) + 
+        (link_to("Register", new_user_registration_path, class: 'nav-link'))
     else
-      link_to("Logout", destroy_user_session_path, method: :delete)
+      link_to("Logout", destroy_user_session_path, method: :delete, class: 'nav-link')
     end
   end
   
