085e92cb0e1a5842ded947e965eb4488b086ef16
Wojtek Kostanski <wojtek@tappest.com>
2018-09-27 16:39:24 +0200

    add petergate rules

--- PortfolioService/app/controllers/posts_controller.rb
+++ PortfolioService/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout 'blog'
+  access all: [:show, :index], user: {expect: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   def index
     @posts = Post.all
