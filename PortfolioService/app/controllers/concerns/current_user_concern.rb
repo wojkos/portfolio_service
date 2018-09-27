cd1e214a5620598f42746fb4d408e495b275c444
Wojtek Kostanski <wojtek@tappest.com>
2018-09-27 20:12:53 +0200

    syntax fixes

--- PortfolioService/app/controllers/concerns/current_user_concern.rb
+++ PortfolioService/app/controllers/concerns/current_user_concern.rb
@@ -1,10 +1,6 @@
 module CurrentUserConcern
   extend ActiveSupport::Concern
 
-  included do
-    before_action :current_user
-  end
-
   def current_user
     super || guest_user
   end
@@ -12,8 +8,8 @@ module CurrentUserConcern
   def guest_user
     guest = GuestUser.new
     guest.name = 'Guest User'
-    guest.first_name = 'Guest',
-    guest.last_name = 'User',
+    guest.first_name = 'Guest'
+    guest.last_name = 'User'
     guest.email = 'asdf@asdf.com'
     guest
   end
