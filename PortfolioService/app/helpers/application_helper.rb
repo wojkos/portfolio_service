d73057b48af27d95b788da58861194ca12a7fde7
Wojtek Kostanski <wojtek@tappest.com>
2018-09-30 22:16:18 +0200

    working on portfolio layout

--- PortfolioService/app/helpers/application_helper.rb
+++ PortfolioService/app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def login_link_helper
+  def login_link_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to("Login", new_user_session_path, class: 'nav-link')) + 
-        (link_to("Register", new_user_registration_path, class: 'nav-link'))
+      (link_to("Login", new_user_session_path, class: style)) + 
+        (link_to("Register", new_user_registration_path, class: style))
     else
-      link_to("Logout", destroy_user_session_path, method: :delete, class: 'nav-link')
+      link_to("Logout", destroy_user_session_path, method: :delete, class: style)
     end
   end
   
@@ -14,6 +14,45 @@ module ApplicationHelper
         "Thanks, for visit from #{session[:source]}"
       end
     end
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blog_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
   
   def copyright_generator
