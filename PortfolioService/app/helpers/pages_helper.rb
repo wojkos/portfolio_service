84eafb2d8318b9d4c03c83af53e8764429601092
Wojtek Kostanski <wojtek@tappest.com>
2018-10-10 16:09:31 +0200

    add twitt parser

--- PortfolioService/app/helpers/pages_helper.rb
+++ PortfolioService/app/helpers/pages_helper.rb
@@ -1,2 +1,39 @@
+#helpers for pages
 module PagesHelper
+  def twitter_parser tweet
+    tweet = link_parser(tweet)
+    hash_parser(tweet)
+  end
+
+  def link_parser(tweet)
+    regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+    tweet.gsub(regex) do |url|
+      "<a href='#{url}' target='_blank'>#{url}</a>"
+    end
+  end
+
+  def hash_parser(tweet)
+    hash_regex = %r{@[a-zA-Z0-9]+}
+
+    tweet.gsub(hash_regex) do |hash|
+      "<b>#{hash}</b>"
+    end.html_safe
+  end
 end
