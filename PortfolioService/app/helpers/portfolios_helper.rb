a523f880fe2a5b856878d0f3f933399a29a70f1e
Wojtek Kostanski <wojtek@tappest.com>
2018-10-10 15:15:17 +0200

    add twitter integration

--- PortfolioService/app/helpers/portfolios_helper.rb
+++ PortfolioService/app/helpers/portfolios_helper.rb
@@ -1,13 +1,13 @@
+# Helpers for portfolio
 module PortfoliosHelper
-
   def image_generator(height, width)
     "http://placehold.it/#{height}x#{width}"
   end
 
-  def portfolio_img img, type
+  def portfolio_img(img, type)
     if img.model.main_image? || img.model.thumb_image?
       img
-    elsif type == 'thumb'      
+    elsif type == 'thumb'
       image_generator(350, 200)
     elsif type == 'main'
       image_generator(600, 400)
--- PortfolioService/config/application.rb
+++ PortfolioService/config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module PortfolioService
   class Application < Rails::Application
+    config.eager_load_paths << "#{Rails.root}/lib"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
