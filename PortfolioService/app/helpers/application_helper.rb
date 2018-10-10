8544c101ec489b79eab1a596ce0836aa1c56f18e
Wojtek Kostanski <wojtek@tappest.com>
2018-10-10 16:34:37 +0200

    finish tech news section

--- PortfolioService/app/helpers/application_helper.rb
+++ PortfolioService/app/helpers/application_helper.rb
@@ -38,6 +38,10 @@ module ApplicationHelper
         url: portfolios_path,
         title: 'Portfolio'
       },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
+      },
     ]
   end
 
